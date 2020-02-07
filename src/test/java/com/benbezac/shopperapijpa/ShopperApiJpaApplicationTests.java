1ffe819174c2d99d803ef13957a360460254e207
BenBezac <benjamin.bezancon@gmail.com>
2020-02-07 09:28:29 +0100

    first commit

---
+++ src/test/java/com/benbezac/shopperapijpa/ShopperApiJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benbezac.shopperapijpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopperApiJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
